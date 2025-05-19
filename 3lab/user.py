be4faec4d70f2fe7a930c8de5e069dfc3a1c8adf
Margarrret <129931635+Margarrret@users.noreply.github.com>
2025-05-19 21:21:59 +0300

    schemas

--- 3lab/user.py
+++ 3lab/user.py
@@ -1,10 +1,14 @@
-from sqlalchemy import Column, Integer, String
-from app.db.base import Base
+from pydantic import BaseModel, EmailStr
 
-class User(Base):
-    __tablename__ = "users"
+class UserBase(BaseModel):
+    email: EmailStr
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+class UserCreate(UserBase):
+    password: str
 
+class User(UserBase):
+    id: int
+
+    model_config = {    # установка конфигурации для модели
+        "from_attributes": True
+    }
