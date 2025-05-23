53a2097f040946580383a889024f8c8a18427d24
Margarrret <129931635+Margarrret@users.noreply.github.com>
2025-05-23 22:37:25 +0300

    Delete 3lab/corpus.py

--- 3lab/corpus.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text
-from app.db.base import Base
-
-class Corpus(Base):
-    __tablename__ = "corpuses"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
-    text = Column(Text, nullable=False)
