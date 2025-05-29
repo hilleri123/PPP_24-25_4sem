3f11338c206263d1651871c99c76a9c5641b3500
Margarrret <129931635+Margarrret@users.noreply.github.com>
2025-05-29 19:37:40 +0300

    Update database.py

--- 3lab/project/app/db/database.py
+++ 3lab/project/app/db/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"  # Или ваша строка подключения
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
 
-Base = declarative_base() # функция, возвращающая базовый класс
 def create_tables():
-    from app.models import user, corpus
     Base.metadata.create_all(bind=engine)
